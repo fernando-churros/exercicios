b8eea22deb940f4cb60ec3f599df01e47242833d
Edward Wong Hau Pepelu Tivrusky IV <fernando_churros@outlook.com>
2026-09-09 00:39:28 -0300

    Update Main.java

--- java/Abstração com Classes e Encapsulamento/Main.java
+++ java/Abstração com Classes e Encapsulamento/Main.java
@@ -1,13 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-
-        ContaBancaria c1 = new ContaBancaria(100);
-        final var pessoa = new ContaBancaria();
-        c1.pagar_boleto(150);
-
-        System.out.printf("saldo: %s%n", c1.getSaldo());
-        System.out.printf("cheque: %s%n", c1.getChequeEspecial());
-        System.out.printf("divida: %s%n", c1.getDivida());
-
     }
 }
